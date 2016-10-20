da71358f402f555c811fd9d72fb3996eb5117123
David Leseten <dleseten@gmail.com>
2016-10-20 11:31:19 -0500

    Get and post routes defined

--- app/routes/growls.js
+++ app/routes/growls.js
@@ -4,7 +4,23 @@ var router = express.Router();
 var Growl = require('../models/growl');
 
 router.get('/', function(req, res, next){
-  console.log("Grrrrrrrrrr.");
+  Growl.find({}, function(err, growls){
+    if (err){
+      res.status(500).send();
+    } else {
+      res.json(growls)
+    }
+  })
+})
+router.post('/', function(req, res, next){
+  var growl = new Growl(req.body);
+  growl.save(function(err){
+    if (err) {
+      res.status(500).send();
+    } else {
+      res.json(growl);
+    }
+  })
 })
 
 
