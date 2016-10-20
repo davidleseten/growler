1b4e02fa22f8b4fc88a6f26723b8855a2f4f58b8
David Leseten <dleseten@gmail.com>
2016-10-20 10:58:09 -0500

    Routes 1

---
+++ app/routes/growls.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const express = require('express');
+var router = express.router();
+var Growl = require('../models/growl');
+
+router.get('/', function(req, res, next){
+  console.log("Grrrrrrrrrr.");
+})
+
+
+module.eports = router;
